8b4018d9240d3cf53baa008c044a7ef037c73b17
MrQin <18748406022@163.com>
2018-06-23 22:59:07 +0800

    add comment for route file

--- routes/test.php
+++ routes/test.php
@@ -1,4 +1,13 @@
 <?php
 use WorkerF\Http\Route;
-
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register routes for your application.
+| Also, you can make a new route file in this Directory and 
+| require it in bootstap/boot.php
+|
+*/
 Route::get('/', "App\Controller\TestController@test");
